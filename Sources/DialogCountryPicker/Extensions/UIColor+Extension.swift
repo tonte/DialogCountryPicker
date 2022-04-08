f8abf6d498e8793fcbf6e6f5b4385c0a23d0a765
Tonte Owuso <tonteowuso@gmail.com>
2022-04-08 02:59:22 +0000

    added spm support

---
+++ Sources/DialogCountryPicker/Extensions/UIColor+Extension.swift
@@ -0,0 +1,29 @@
+import Foundation
+import UIKit
+extension UIColor {
+    static let primaryDark = UIColor(hexString: "#061F3C")
+    static let secondaryDark = UIColor(hexString: "#051C3D", alpha: 0.91)
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+}
---
+++ Tests/DialogCountryPickerTests/DialogCountryPickerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DialogCountryPicker
+
+final class DialogCountryPickerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DialogCountryPicker().text, "Hello, World!")
+    }
+}
